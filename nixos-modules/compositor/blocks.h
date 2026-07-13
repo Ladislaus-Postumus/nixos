d169a937198412c9d32911393daa2636e410bc65
Philipp Melzer <philipp.melzer2014+git@gmail.com>
2026-07-13 14:51:27 +0200

    fix dwm to use native script locations

--- nixos-modules/compositor/blocks.h
+++ nixos-modules/compositor/blocks.h
@@ -1,13 +1,13 @@
 static const Block blocks[] = {
-	/* Icon  */ /* Command */                         /* Interval */ /* Signal */
-	{ "", "/home/pme/.local/bin/status/volume.sh",        0,            1 },
-	{ "", "/home/pme/.local/bin/status/network.sh",       10,           2 },
-	{ "", "/home/pme/.local/bin/status/bluetooth.sh",     10,           3 },
-	{ "",      "/home/pme/.local/bin/status/cpu.sh",      5,            5 },
-	{ "",      "/home/pme/.local/bin/status/mem.sh",      5,            5 },
-	{ "",      "/home/pme/.local/bin/status/gpu.sh",      5,            5 },
+    /* Icon  */ /* Command */ /* Interval */ /* Signal */
+    {"", "volume-stat", 1, 1},
+    {"", "network-stat", 5, 2},
+    {"", "bluetooth-stat", 10, 3},
+    {"", "cpu-stat", 1, 5},
+    {"", "mem-stat", 1, 5},
+    {"", "gpu-stat", 1, 5},
 
-	{ "",      "date '+%H:%M %Y-%m-%d'",                  30,           0 },
+    {"", "date '+%H:%M %Y-%m-%d'", 30, 0},
 };
 
 static char delim[] = " | ";
